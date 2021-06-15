bfd7470e962f2b921005d5ee4deaa3c5c61225d5
Hagen DeRouen <hagend87@gmail.com>
2021-06-15 15:50:19 -0500

    Finish Gatsby tutorial Part 4

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,26 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
+import { siteTitle } from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
+    const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }  
+  `)
   return (
     <main>
-      <title>{pageTitle}</title>
+      <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+      <p className={siteTitle}>{data.site.siteMetadata.title}</p>
       <nav>
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
+          <li><Link to="/blog">Blog</Link></li>
         </ul>
       </nav>
       <h1>{pageTitle}</h1>
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,6 @@ import { StaticImage } from 'gatsby-plugin-image'
 const IndexPage = () => {
   return (
     <main>
-      <title>Home Page</title>
       <h1>Welcome to my Gatsby site!</h1>
       <Layout pageTitle={"Home Page"}>
         <p>I'm making this by following the Gatsby Tutorial.</p>
